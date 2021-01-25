248be5f09773be9cf56c8293f1b62eef9c4150a3
sinamna <sina3050@gmail.com>
2021-01-25 17:26:38 +0330

    masalan done fan speed

--- Module-3/FanSpeed.v
+++ Module-3/FanSpeed.v
@@ -26,9 +26,26 @@ module FanSpeed (
 	input  [7:0] speed    , // speed [duty-cycle]  
 	output       pwm_data   // data  [output]
 );
-
+	reg data;
+	reg count <= speed;
+	integer i;
 	/* write your code here */
-	
+	always @(negedge arst or posedge clk) begin
+		if(~arst ) pwm_data=1'b0;
+		else
+			begin
+			for(i=0;i<=256;i=i+1)
+				begin
+					if(count >=0)
+					data = 1'b1;
+					else
+					data = 1'b0;
+				end
+			end
+		
+	end
+
+	assign pwm_data = data;
 	/* write your code here */
 
 endmodule
